27962045b9951c3abfa0ab4f7f8ce8954dad5e49
J Rose <josh@upros3.com>
2018-01-17 20:01:37 +0000

    Completed exercise

--- character-positioning.js
+++ character-positioning.js
@@ -1,14 +1,13 @@
 function characterPositioning(sentence) {
-  //finds letters only and splits sentence to make it ready for the loop to push into object.
-  var sentence = sentence.replace(/\s+/g, '').split('');
+  var sentence = sentence.split('');
   var positioning = {};
   for (var i = 0; i < sentence.length; i++) {
     var index = [];
     index += sentence[i];
     if (positioning[sentence[i]]) {
-      positioning[sentence[i]]
-    } else {
-      positioning[sentence[i]] = 3;
+      positioning[sentence[i]].push(i);
+    } else if (sentence[i] != ' ') {
+        positioning[sentence[i]] = [i];
     }
 
   } return positioning;
